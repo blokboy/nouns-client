6324f4797411e4370f5ddb4e930cac11c1a76509
joe lazarz <jlazarz87@gmail.com>
2022-07-09 14:00:19 -0400

    prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
-import type { NextPage } from "next";
-import Image from "next/image";
-import { motion } from "framer-motion";
+import type { NextPage } from "next"
+import Image from "next/image"
+import { motion } from "framer-motion"
 
 const Home: NextPage = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <motion.div
-        initial={{ rotate: 0 }}
-        animate={{ rotate: 360 }}
-        transition={{ duration: 4, repeat: Infinity }}
-      >
+      <motion.div initial={{ rotate: 0 }} animate={{ rotate: 360 }} transition={{ duration: 4, repeat: Infinity }}>
         <Image src="/nouns-icon.png" alt="nouns" height="100px" width="100px" />
       </motion.div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
