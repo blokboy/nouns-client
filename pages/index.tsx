d3e4547ddc55d45846917d11f7587fafbe5ac5b7
Théo Mode <milesxbridges@gmail.com>
2022-07-11 08:41:12 -0400

    add current auction and nouns contract to store, init current auction react

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
 import type { NextPage } from "next"
 import Image from "next/image"
 import { motion } from "framer-motion"
-import Auction from "components/Auction/Auction"
 
 const Home: NextPage = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <motion.div initial={{ rotate: 0 }} animate={{ rotate: 360 }} transition={{ duration: 4, repeat: Infinity }}>
         <Image src="/nouns-icon.png" alt="nouns" height="100px" width="100px" />
-        <Auction />
       </motion.div>
     </div>
   )
--- stores/useAuctionStore.tsx
+++ stores/useAuctionStore.tsx
@@ -1,15 +1,20 @@
-import create from 'zustand'
+import create from "zustand"
+import { AuctionProps } from "typings/auction"
 
 interface AuctionStoreProps {
   nounsGraphClient: {} | null
   setNounsGraphClient: (client: any) => void
-  currentAuction: {} | null
-  setCurrentAuction: (currentAuction: {}) => void
+  currentAuction: AuctionProps | null
+  setCurrentAuction: (currentAuction: AuctionProps) => void
+  nounsContract: {} | null
+  setNounsContract: (nounsContract: {}) => void
 }
 
-export const useAuctionStore = create<AuctionStoreProps>((set) => ({
+export const useAuctionStore = create<AuctionStoreProps>(set => ({
   nounsGraphClient: null,
-  setNounsGraphClient: (nounsGraphClient) => set({ nounsGraphClient }),
+  setNounsGraphClient: nounsGraphClient => set({ nounsGraphClient }),
   currentAuction: null,
-  setCurrentAuction: (currentAuction) => set({ currentAuction})
+  setCurrentAuction: currentAuction => set({ currentAuction }),
+  nounsContract: null,
+  setNounsContract: nounsContract => set({ nounsContract }),
 }))
