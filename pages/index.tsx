96a34dec51486caa1e654b4f84948a0a3d7c1c75
Théo Mode <milesxbridges@gmail.com>
2022-07-11 11:08:11 -0400

    fix hydration issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
+import React from "react"
 import type { NextPage } from "next"
 import Image from "next/image"
 import { motion } from "framer-motion"
 import Auction from "components/Auction/Auction"
 import { useNounsAuctions } from "hooks/useNounsAuctions"
+import { AuctionProps } from "../typings/auction"
 
 const Home: NextPage = () => {
   const { currentAuction } = useNounsAuctions()
+  const [current, setCurrent] = React.useState<AuctionProps | null>(null)
+
+  //hacky?
+  React.useEffect(() => {
+    if (!currentAuction) return
+
+    setCurrent(currentAuction)
+  }, [currentAuction])
 
   return (
     <div className="flex justify-center items-center h-screen">
-      {(!!currentAuction && <Auction />) || (
+      {(!!current && <Auction />) || (
         <motion.div initial={{ rotate: 0 }} animate={{ rotate: 360 }} transition={{ duration: 4, repeat: Infinity }}>
           <Image src="/nouns-icon.png" alt="nouns" height="100px" width="100px" />
         </motion.div>
