ee9f6756950afedae7a09679650243ab57885f4f
blokboy <proxxist@gmail.com>
2022-07-12 22:39:36 -0400

    sending up start of DAO cards when a user signs in

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,10 @@ import Image from "next/image"
 import { motion } from "framer-motion"
 import { AuctionProps } from "../typings/auction"
 import Auction from "../components/Auction/Auction"
+import DaoCard from "components/DaoCard/DaoCard"
 import { useNounsAuctions } from "../hooks/useNounsAuctions"
+import { useLayoutStore } from "stores/useLayoutStore"
+import { useSigner } from "wagmi"
 
 const Home: NextPage = () => {
   const { currentAuction } = useNounsAuctions()
@@ -19,7 +22,7 @@ const Home: NextPage = () => {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      {(!!current && <Auction />) || (
+      {(!!current && <DaoCard addr="0xA699720418F612558E2827Bf68631686e9d1095e"/>) || (
         <motion.div initial={{ rotate: 0 }} animate={{ rotate: 360 }} transition={{ duration: 4, repeat: Infinity }}>
           <Image src="/nouns-icon.png" alt="nouns" height="100px" width="100px" />
         </motion.div>
